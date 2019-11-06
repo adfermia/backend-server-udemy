c7c8b7339360ff425458b5ea5cb9b1e322c711e8
adrian <adrianfern95@gmail.com>
2019-11-06 17:32:58 +0100

    Seccion 12 terminada

--- config/config.js
+++ config/config.js
@@ -3,3 +3,7 @@
 
 
 module.exports.SEED = '$adrian$sargantana$master';
+
+
+// Google
+module.exports.CLIENT_ID ='912787213017-um1htis38j7tog0k3juot839ousr73il.apps.googleusercontent.com'
--- routes/app.js
+++ routes/app.js
@@ -8,4 +8,10 @@ app.get('/', (req, res, next) => {
     res.status(200).json({ ok: true, mensaje: 'Peticion realizada correctamente'});
 });
 
+// Id de cliente
+// 912787213017-um1htis38j7tog0k3juot839ousr73il.apps.googleusercontent.com
+
+
+// Id secreto de cliente
+// xZOnvqP8q4yMW8vGgpAlz7fS
 module.exports = app;
